b4bb1574d1aa3e644928827bade65c2afda03b41
ManeeshDevanaboyina <32562977+ManeeshDevanaboyina@users.noreply.github.com>
2022-11-30 15:15:03 -0600

    Filtered only English Tweets

--- kafka/producer.py
+++ kafka/producer.py
@@ -5,8 +5,10 @@ from kafka import KafkaProducer
 from json import dumps
 from datetime import datetime, timedelta
 
-
-#bearer_token = "AAAAAAAAAAAAAAAAAAAAAAkajwEAAAAACoklzU5Y6z%2BQfSDuqJbWpOmOvPU%3D115RjOCiJ75DIP9vSbHSFxoC65K6xheIdgB0uxJJ8IxNvTmlPE"
+def normalize_timestamp(time):
+    mytime = datetime.strptime(time, "%Y-%m-%d %H:%M:%S")
+    mytime += timedelta(hours=1)   # the tweets are timestamped in GMT timezone, while I am in +1 timezone
+    return (mytime.strftime("%Y-%m-%d %H:%M:%S"))
 
 with open('config.json') as json_file:
     data = json.load(json_file)
@@ -16,10 +18,12 @@ client = tweepy.Client(bearer_token)
 # Search Recent Tweets
 
 # This endpoint/method returns Tweets from the last seven days
-
-response = client.search_recent_tweets("iphone OR Musk")
+# By default, this endpoint/method returns 10 results
+# You can retrieve up to 100 Tweets by specifying max_results
+response=client.search_recent_tweets(query="iphone OR Musk",max_results=10,tweet_fields=['created_at','lang'],expansions=['author_id'])
+#print(response)
+#response=client.get_all_tweets_count("iphone")
 response1=client.search_recent_tweets("Covid")
-#print(reponse1)
 # The method returns a Response object, a named tuple with data, includes,
 # errors, and meta fields
 #print(response.meta)
@@ -27,7 +31,7 @@ response1=client.search_recent_tweets("Covid")
 # In this case, the data field of the Response returned is a list of Tweet
 # objects
 tweets = response.data
-newtopic_tweets=response1.data
+new_topic_tweets = response1.data
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'],value_serializer=lambda K:dumps(K).encode('utf-8'))
 topic_name = 'Twitter-Kafka'
 topic_name1="Covid"
@@ -35,15 +39,17 @@ topic_name1="Covid"
 # Each Tweet object has default ID and text fields
 def get_twitter_data1():
     for tweet in tweets:
-
-        producer.send(topic_name, tweet.text)
-        #print(tweet.id)
-        print(tweet.text)
+        if(tweet.lang=='en'):
+            producer.send(topic_name,tweet.text)
+            #print(tweet.id)
+            print(tweet.lang)
+            print(tweet.text)
 
 def get_twitter_data2():
-    for tweet in newtopic_tweets:
+    for tweet in new_topic_tweets:
 
-        producer.send(topic_name1, tweet.text)
+        producer.send(topic_name1, tweet.id)
+        #print(str(normalize_timestamp(str(tweet.created_at))))
         #print(tweet.id)
         print(tweet.text)
 
@@ -51,9 +57,8 @@ def get_twitter_data2():
 #get_twitter_data1()
 
 
-# By default, this endpoint/method returns 10 results
-# You can retrieve up to 100 Tweets by specifying max_results
-response = client.search_recent_tweets("Tweepy", max_results=100)
+
+
 
 def periodic_work(interval):
     while True:
